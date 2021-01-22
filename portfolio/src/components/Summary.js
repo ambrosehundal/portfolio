2c2cd0de387eb075f75535da2d47b32b90c79e65
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-21 22:38:25 -0800

    summary component created

---
+++ portfolio/src/components/Summary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Summary(props){
+    return (
+        <div>
+            <h2>Hi my name is Ambrose. I am a passionate developer and I love to build.</h2>
+        </div>
+    )
+};
+
+
+export default Summary
