937f096fec2526d280f5a426a9f59335c93a69e1
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-23 19:50:26 -0800

    added props to project component

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,4 +1,19 @@
 import React from 'react'
 
 
+const Projects = ({name, description, tech_stack, project_link}) => {
+    return (
+        <div>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <h6>{tech_stack}</h6>
+            <p>{project_link}</p>
+        
+        </div>
+    )
 
+}
+
+
+
+export default Projects
