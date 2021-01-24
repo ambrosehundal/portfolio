96a007c1f81d22a54f8444f6a4f88b85cdf440de
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-23 19:55:22 -0800

    first project added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Summary from './components/Summary'
+import Projects from './components/projects'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
 
         <Summary/>
+        <Projects name="Proscout" description="A networking platform for MMA athetes to find training partners and connect with coaches for their fights"/>
         
       </header>
     </div>
