da35acf16c6ee66fad1bb9fa6bd21e5bca1c40c1
Ambrose Hundal <amshunda@ucsc.edu>
2021-01-26 20:49:47 -0800

    hooks implemented in summary component

--- portfolio/src/components/Summary.js
+++ portfolio/src/components/Summary.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function Summary(props){
+
+const [dollars, setCount] = useState(1000);
+
     return (
         <div>
             <h2>Hi my name is Ambrose. I am a passionate developer and I love to build.</h2>
+            <button onClick={ () => setCount(dollars+1000)}>Lets make more money $$$  </button>
+
+            <h6>By the time I turn 30, I hope to make {dollars} dollars</h6>
         </div>
     )
 };
